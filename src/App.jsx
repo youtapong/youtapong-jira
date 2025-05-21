3f7670971f1d1c4d7236ad8c2a7a605f8b5cb453
youtapong <youtapong@gmail.com>
2025-05-21 14:01:06 +0700

    add loing

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>E-Com System 1.0.0</h1>
+      <h1>E-Com System 1.0.0 (add login page)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
